c17098bb52cedf2d927614ff26b78bdcf117c4a6
Иван <zierg@yandex.ru>
2014-04-06 19:52:11 +0400

    Класс для исправления глюков кодировки

---
+++ src/java/pack/EncodingConverter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author Ivan
+ */
+public class EncodingConverter {
+    /**
+     * Чтобы писать меньше. Конвертирует из ISO-8859-1 в UTF-8.
+     * @param source исходная строка
+     * @return строку в кодировке UTF-8
+     */
+    public static String convert(String source) {
+        return convertFromISO88591(source);
+    }
+    
+    /**
+     * Конвертирует из ISO-8859-1 в UTF-8.
+     * @param source исходная строка в кодировке ISO-8859-1
+     * @return строку в кодировке UTF-8 
+     */
+    public static String convertFromISO88591(String source) {
+        try {
+            return new String(source.getBytes("ISO-8859-1"), "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            return source;
+        }
+    }
+}
