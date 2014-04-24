9868136eb9313f83b559552aba2e9a5a3fef9e69
Иван <zierg@yandex.ru>
2014-04-24 11:52:49 +0400

    Все пути сервлетов теперь в классе PathConstants

---
+++ src/java/pack/PathConstants.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack;
+
+/**
+ *
+ * @author Ivan
+ */    
+
+public interface PathConstants {
+    String CLIENT_HOME = "/clientHome";
+    String LOGIN = "/Login";
+    String LOGOUT = "/Logout";
+    String REMOVE_SERVICE_FROM_TARIFF = "/RemoveServiceFromTariff";
+    String SHOW_TARIFF = "/ShowTariff";
+    String TARIFF_FILTER = "/TariffFilter";
+    String TARIFF_ADD_FORM = "/TariffAddForm";
+    String TARIFF_UPDATE_FORM = "/TariffUpdateForm";
+    String TARIFF_DELETE = "/TariffDelete";
+    String SELECT_ALL_TARIFF = "/SelectAllTariff";
+    String REMOVE_SERVICE_FROM_SIM = "/RemoveServiceFromSim";
+    String CHOOSE_SIM = "/ChooseSim";
+    String ADD_SERVICE_TO_SIM = "/AddServiceToSim";
+    String SERVICE_UPDATE_FORM = "/ServiceUpdateForm";
+    String SERVICE_ADD_FORM = "/ServiceAddForm";
+    String SERVICE_FILTER = "/ServiceFilter";
+    String SERVICE_UPDATE = "/ServiceUpdate";
+    String SERVICE_DELETE = "/ServiceDelete";
+    String SERVICE_ADD = "/ServiceAdd";
+    String SELECT_ALL_SERVICE = "/SelectAllService";
+}
